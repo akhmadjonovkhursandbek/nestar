09ca9f23f7dcbde5c114d2e6427880fbdb0e8d46
Token <axmadjonovxursandbek7@gmail.com>
2025-03-25 08:34:20 +0900

    fix: typescript eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,24 +6,16 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	{
-		ignores: ['eslint.config.mjs'],
-	},
+	{ ignores: ['eslint.config.mjs'] },
 	eslint.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
 	eslintPluginPrettierRecommended,
 	{
 		languageOptions: {
-			globals: {
-				...globals.node,
-				...globals.jest,
-			},
+			globals: { ...globals.node, ...globals.jest },
 			ecmaVersion: 5,
 			sourceType: 'module',
-			parserOptions: {
-				projectService: true,
-				tsconfigRootDir: import.meta.dirname,
-			},
+			parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
 		},
 	},
 	{
